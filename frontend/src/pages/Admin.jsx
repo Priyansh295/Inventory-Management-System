bad6158b170d8e886e880f7bdc448a73046e221b
GayathriManoj2003 <gayathrimanoj2103@gmail.com>
2023-11-13 12:02:01 +0530

    Update Files

--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -1,13 +1,44 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { AuthContext } from '../context/authContext';
 // import { Link } from 'react-router-dom';
+import "./Admin.scss"
+import Orders from './Orders';
+import Products from './Products';
 
 const Admin = () => {
     const {admin} = useContext(AuthContext);
+    const [selectedOption, setSelectedOption] = useState([])
+    function handleClick(e) {
+      const buttonName = e.target.name;
+      setSelectedOption(buttonName);
+    }
+    const renderSelectedComponent = () => {
+      switch (selectedOption) {
+        case 'Orders':
+          return <Orders/>;
+        case 'Products':
+          return <Products/>;
+        // Add other cases for different components
+        default:
+          return null;
+      }
+    }
     if (admin) {
     return (
-      <div> <h1>Admin</h1>
-
+      <div className='admin_container'>
+          <h1>Admin</h1>
+          <div className='options'>
+            <div className='buttons'>
+              <button name = "Orders" onClick={handleClick}>Orders</button>
+              <button name = "Suppliers" onClick={handleClick}>Suppliers</button>
+              <button name = "Products" onClick={handleClick}>Products</button>
+              <button name = "Storage" onClick={handleClick}>Storage</button>
+              <button name = "Employee" onClick={handleClick}>Employees</button>
+            </div>
+            <div className='populate'>
+            {renderSelectedComponent()}
+            </div>
+          </div>
       </div>
 
     )
