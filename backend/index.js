2facc78f56938ca8bb429ed115c02bbc4e738e1e
GayathriManoj2003 <gayathrimanoj2103@gmail.com>
2023-11-14 18:42:35 +0530

    Add bodyParser

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ import express from "express";
 import cors from "cors";
 import routes from "./routes.js";
 import cookieParser from "cookie-parser";
+import db from "./db.js"
+import bodyParser from 'body-parser'
 
 const app = express();
 const port = 8800;
@@ -10,9 +12,13 @@ const port = 8800;
 app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
+app.use(bodyParser.json());
 app.use("/", routes);
 
 app.listen(port, () => {
     console.log("Server up and running @", port);
     console.log(`URL: http://localhost:${port}/`);
 });
+
+
+
