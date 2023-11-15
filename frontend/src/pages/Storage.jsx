53c86f327b9d1afde9aa3972b7c0d8676139fabb
GayathriManoj2003 <gayathrimanoj2103@gmail.com>
2023-11-15 16:34:07 +0530

    Admin - Storage CRUD Functionality

--- frontend/src/pages/Storage.jsx
+++ frontend/src/pages/Storage.jsx
@@ -1,9 +1,120 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../context/authContext';
+import axios from 'axios';
+import './Storage.scss'; // Import your SCSS file
+import {StoreModal, StoreAddModal} from './StorageModal'; // Import the StoreModal component
 
 const Storage = () => {
-  return (
-    <div>Storage</div>
-  )
-}
+  const { admin } = useContext(AuthContext);
+  const [stores, setStores] = useState([]);
+  const [selectedStore, setSelectedStore] = useState(null);
+  const [isUpdateModalOpen, setIsUpdateModalOpen] = useState(false);
+  const [isAddModalOpen, setIsAddModalOpen] = useState(false); // New state for the add modal
 
-export default Storage
+  useEffect(() => {
+    fetchStores();
+  }, []);
+  const [err, setError] = useState([])
+  const [msg, setMsg] = useState([])
+  const fetchStores = async () => {
+    try {
+      const res = await axios.get(`http://localhost:8800/stores`);
+      setStores(res.data);
+      console.log(res.data)
+    } catch (error) {
+      console.error('Error fetching stores:', error);
+    }
+  };
+
+  const handleAdd = () => {
+    setMsg(null)
+    setIsAddModalOpen(true);
+  };
+
+  const handleDelete = async (store) => {
+    try {
+      const Store_id = store.Store_id;
+      console.log(Store_id);
+      const res = await axios.delete("http://localhost:8800/stores/"+Store_id)
+      console.log(res)
+      fetchStores();
+      setMsg(res.data)
+    } catch (err) {
+      console.log(err);
+      setError(err.response.data);
+    }
+  };
+
+  const openUpdateModal = (store) => {
+    setMsg(null)
+    setSelectedStore(store);
+    setIsUpdateModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setSelectedStore(null);
+    setIsUpdateModalOpen(false);
+    setIsAddModalOpen(false); // Close the add modal
+    fetchStores();
+  };
+
+  if (admin) {
+    return (
+      <div className='store_container'>
+        <h1>Stores</h1>
+        <div className="store-table">
+          <button className="add-button" onClick={handleAdd}>
+            Add Store
+          </button>
+          {err && <p> {err}</p>}
+          {msg && <p> {msg}</p>}
+          <table>
+            <thead>
+              <tr>
+                <th>Store ID</th>
+                <th>Part ID</th>
+                <th>Quantity</th>
+                <th>Rack_no</th>
+                <th>Block_no</th>
+                <th>Theshold</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {stores.map((store) => (
+                <tr className="store_row" key={store.Store_id}>
+                  <td>{store.Store_id}</td>
+                  <td>{store.Part_id}</td>
+                  <td>{store.Quantity}</td>
+                  <td>{store.Rack_no}</td>
+                  <td>{store.Block_no}</td>
+                  <td>{store.Threshold}</td>
+                  <td className='store-buttons'>
+                    <button className="update-button" onClick={() => openUpdateModal(store)}>
+                      Update
+                    </button>
+                    <button className="delete-button" onClick={() => handleDelete(store)}>
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+
+          {/* Conditionally render the modals */}
+          {isUpdateModalOpen && (
+            <StoreModal isOpen={isUpdateModalOpen} onClose={closeModal} store={selectedStore} />
+          )}
+          {isAddModalOpen && (
+            <StoreAddModal isOpen={isAddModalOpen} onClose={closeModal} />
+          )}
+        </div>
+      </div>
+    );
+  } else {
+    return <></>;
+  }
+};
+
+export default Storage;
---
+++ frontend/src/pages/StorageModal.jsx
@@ -0,0 +1,263 @@
+// StorageModal.jsx
+
+import React, { useEffect, useState } from 'react';
+import "./SupplierModal.scss"
+import axios from 'axios';
+
+export const StoreModal = ({ isOpen, onClose, store }) => {
+  const [updatedStore, setUpdatedStore] = useState({
+    Store_id: store.Store_id,
+    Quantity: store.Quantity,
+    Rack_no: store.Rack_no,
+    Block_no: store.Block_no,
+    Threshold: store.Threshold,
+  });
+
+  const [msg, setMsg] = useState([])
+  const [err, setError] = useState([])
+
+  const handleInputChange = (e) => {
+    setError(null);
+
+    const { name, value, type } = e.target;
+    const convertedValue = type === 'number' ? parseFloat(value) : value;
+    setUpdatedStore((prevStore) => ({
+      ...prevStore,
+      [name]: convertedValue,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError(null);
+
+    console.log('Updated Store:', updatedStore);
+    try {
+      const Store_id = store.Store_id;
+      console.log(Store_id);
+      const res = await axios.put("http://localhost:8800/stores/"+Store_id, updatedStore)
+      console.log(res)
+      setMsg(res.data)
+    } catch (err) {
+      console.log(err);
+      setError(err.message);
+    }
+  };
+
+  return (
+    <div className={`modal ${isOpen ? 'open' : ''}`} id="modalcontainer">
+      <div className="modal-content">
+        <span className="close-btn" onClick={onClose}>&times;</span>
+        <h2>Edit Store</h2>
+        <form onSubmit={handleSubmit}>
+          <strong>Store_id  - {store.Store_id}</strong>
+          <label className="label" htmlFor="storeName">
+            <span>Part_id:</span>
+            <input required disabled
+              type="text"
+              id="Part_id"
+              name="Part_id"
+              value={store.Part_id}
+            />
+          </label>
+          <label className="label">
+            <span>Quantity:</span>
+            <input required
+              type="number"
+              id="Quantity"
+              name="Quantity"
+              min = "1"
+              defaultValue={updatedStore.Quantity}
+              onChange={handleInputChange}
+            />
+          </label>
+          <label className="label">
+            <span>Rack Number:</span>
+            <input required
+              type= "text"
+              id="Rack_no"
+              name="Rack_no"
+              defaultValue={updatedStore.Rack_no}
+              onChange={handleInputChange}
+            />
+          </label>
+          <label className="label">
+            <span>Block Number:</span>
+            <input required
+              type="text"
+              id="Block_no"
+              name="Block_no"
+              defaultValue={updatedStore.Block_no}
+              onChange={handleInputChange}
+            />
+          </label>
+          <label className="label">
+            <span>Theshold</span>
+            <input required
+              type="number"
+              id="Threshold"
+              name="Threshold"
+              defaultValue={updatedStore.Threshold}
+              onChange={handleInputChange}
+            />
+          </label>
+          {msg && <p> {msg}</p>}
+          {err && <p> {err}</p>}
+          <button type="submit">
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export const StoreAddModal = ({ isOpen, onClose }) => {
+  const [newStore, setNewStore] = useState({
+    Store_id: '',
+    Part_id: '',
+    Quantity: '',
+    Rack_no: '',
+    Block_no: '',
+    Threshold: 0,
+  });
+  const [parts, setParts] = useState([]);
+  const [msg, setMsg] = useState([]);
+  const [err, setError] = useState([]);
+
+  const handleInputChange = (e) => {
+    setError(null);
+    const { name, value, type } = e.target;
+    const convertedValue = type === 'number' ? parseFloat(value) : value;
+    setNewStore((prevStore) => ({
+      ...prevStore,
+      [name]: convertedValue,
+    }));
+  };
+
+  const fetchParts = async () => {
+    try {
+      const res = await axios.get('http://localhost:8800/parts');
+      console.log(res.data);
+      setParts(res.data);
+    } catch (err) {
+      console.log(err);
+      // setError(err.response.data);
+    }
+  };
+
+  useEffect(() => {
+    fetchParts();
+  }, []);
+
+  const handlePartSelectChange = (e) => {
+    setNewStore((prevStore) => ({
+      ...prevStore,
+      Part_id: e.target.value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError(null);
+
+    try {
+      const res = await axios.post('http://localhost:8800/stores/add', newStore);
+      console.log(res?.data || "Success");
+      setMsg(res?.data)
+    } catch (err) {
+      setError(err.response?.data);
+    }
+  };
+
+  return (
+    <div className={`modal ${isOpen ? 'open' : ''}`} id="modalcontainer">
+      <div className="modal-content">
+        <span className="close-btn" onClick={onClose}>
+          &times;
+        </span>
+        <h2>Add New Store</h2>
+        <form onSubmit={handleSubmit}>
+          <label className="label" htmlFor="storeID">
+            <span>Store ID:</span>
+            <input
+              required
+              type="text"
+              id="storeID"
+              name="Store_id"
+              defaultValue={newStore.Store_id}
+              onChange={handleInputChange}
+            />
+          </label>
+          <label className="label">
+            <span>Part ID:</span>
+            <select
+              required
+              id="partID"
+              name="Part_id"
+              value={newStore.Part_id}
+              onChange={handlePartSelectChange}
+            >
+              <option value="" disabled>
+                Select Part ID
+              </option>
+              {parts.map((part) => (
+                <option key={part.Part_id} value={part.Part_id}>
+                  {part.Part_id}
+                </option>
+              ))}
+            </select>
+          </label>
+          <label className="label">
+            <span>Quantity:</span>
+            <input
+              required
+              type="text"
+              id="Quantity"
+              name="Quantity"
+              defaultValue={newStore.Quantity}
+              onChange={handleInputChange}
+            />
+          </label>
+          <label className="label">
+            <span>Rack Number:</span>
+            <input
+              required
+              type="text"
+              id="Rack_no"
+              name="Rack_no"
+              defaultValue={newStore.Rack_no}
+              onChange={handleInputChange}
+            />
+          </label>
+          <label className="label">
+            <span>Block Number:</span>
+            <input
+              required
+              type="text"
+              id="Block_no"
+              name="Block_no"
+              defaultValue={newStore.Block_no}
+              onChange={handleInputChange}
+            />
+          </label>
+          <label className="label">
+            <span>Threshold:</span>
+            <input
+              required
+              type="number"
+              id="Threshold"
+              name="Threshold"
+              min="0"
+              defaultValue={newStore.Threshold}
+              onChange={handleInputChange}
+            />
+          </label>
+          {msg && <p>{msg}</p>}
+          {err && <p>{err}</p>}
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </div>
+  );
+};
