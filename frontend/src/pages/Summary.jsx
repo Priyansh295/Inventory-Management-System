86449a10f3a98bb824573903fe45c48111b5b017
GayathriManoj2003 <gayathrimanoj2103@gmail.com>
2023-11-21 08:54:54 +0530

    Page For Summary Statistics

---
+++ frontend/src/pages/Summary.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const Summary = () => {
+  return (
+    <div className='Summary'>
+        <h1>Summary</h1>
+        <div>
+            <div>
+                Total Products Sold
+            </div>
+            <div>
+                Clients Catered To
+            </div>
+            <div>
+                Suppliers Orders
+            </div>
+            <div>
+                Employees
+            </div>
+            <div>
+                Total Orders
+            </div>
+            <div>
+                Completed Orders
+            </div>
+            <div>
+                Orders In Progress
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Summary
